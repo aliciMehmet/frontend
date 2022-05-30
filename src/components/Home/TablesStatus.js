1e88735332b408dff59b24fefd61685bf5ce9a98
Emrullah <emrullahsevmisss@gmail.com>
2022-05-30 18:28:52 +0300

    fix

--- src/components/Home/TablesStatus.js
+++ src/components/Home/TablesStatus.js
@@ -2,29 +2,34 @@ import React, { useEffect, useState } from 'react'
 import { AuthContext } from '../../context/AuthContext';
 import BusinessService from '../../services/BusinessService';
 import Home from '../Home';
-import "./TablesStatus.css"
 
 function TablesStatus() {
     const {user} = React.useContext(AuthContext)
-    const [tables,setTables] = useState([])
+    const [tables,setTables] = useState(new Map())
 
     useEffect(() => {
         let businessService = new BusinessService();
         businessService.getTablesStatus(user.token).then(result => {
-          console.log(result.data.data)
-          setTables(result.data.data)
 
+          if (result.data != null) {
+            let map = new Map()
+            Object.keys(result.data.data).forEach(key =>{
+                map.set(key, result.data.data[key])
+            })
+
+            setTables(map)
+
+          }
         })
       }, []);
   return (
-    <div id='table_container'>
-      {tables.map(table => {
-        return (
-          <div style={{
-            backgroundColor: table.status ? 'red' : 'rgb(99, 194, 80)'
-          }} className='table'>{table.tableId}</div>
-        )
-      })}
+    <div>
+    {tables.map((value,key,map) => {
+      return  <Home />
+         
+      
+         
+    })}
     </div>
   )
 }
