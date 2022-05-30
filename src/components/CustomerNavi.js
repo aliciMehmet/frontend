18f83e70adaf9d157a173da5802b7fe211f3a666
Emrullah <emrullahsevmisss@gmail.com>
2022-05-30 21:14:22 +0300

    tba

--- src/components/CustomerNavi.js
+++ src/components/CustomerNavi.js
@@ -2,10 +2,7 @@ import React,{Component} from 'react';
 import {NavLink} from 'react-router-dom';
 import {Navbar,Nav} from 'react-bootstrap';
 
-
-
-
-export class BusinessNavi extends Component
+export class CustomerNavi extends Component
 {
 	render()
 	{
@@ -17,16 +14,11 @@ export class BusinessNavi extends Component
 				<Navbar.Collapse id="basic-navbar-nav">
 				<Nav className='d-flex justify-content-center'>
 					<NavLink className="d-inline p-2 bg-dark text-white"
-					to="/sdfsdf">Manage Items</NavLink>
+					to="/">Get Receipt</NavLink>
 					<NavLink className="d-inline p-2 bg-dark text-white"
-					to="/onlineWaiters">Online Waiters</NavLink>
+					to="/">Call A Waiter</NavLink>
 					
-					<NavLink className="d-inline p-2 bg-dark text-white"
-					to="/tablesStatus">Tables Status</NavLink>
-					<NavLink className="d-inline p-2 bg-dark text-white"
-					to="/manageEmployees">Manage Employees</NavLink>
-					<NavLink className="d-inline p-2 bg-dark text-white"
-					to="/allOrders">All Orders</NavLink>
+					
 				</Nav>
 				</Navbar.Collapse>
 
--- src/components/Home/TablesStatus.js
+++ src/components/Home/TablesStatus.js
@@ -1,31 +1,62 @@
 import React, { useEffect, useState } from 'react'
+import { Card, ListGroup, Row } from 'react-bootstrap';
 import { AuthContext } from '../../context/AuthContext';
 import BusinessService from '../../services/BusinessService';
+import { BusinessNavi } from '../BusinessNavi';
 import Home from '../Home';
-import "./TablesStatus.css"
+
 
 function TablesStatus() {
-    const {user} = React.useContext(AuthContext)
-    const [tables,setTables] = useState([])
+  const { user } = React.useContext(AuthContext)
+  const [tables, setTables] = useState([])
+  let a;
 
-    useEffect(() => {
-        let businessService = new BusinessService();
-        businessService.getTablesStatus(user.token).then(result => {
-          console.log(result.data.data)
-          setTables(result.data.data)
+  useEffect(() => {
+    let businessService = new BusinessService();
+    businessService.getTablesStatus(user.token).then(result => {
+      console.log(result.data.data)
+      setTables(result.data.data)
 
-        })
-      }, []);
+    })
+  }, []);
   return (
-    <div id='table_container'>
-      {tables.map(table => {
-        return (
-          <div style={{
+    <div>
+      <BusinessNavi />
+      <div className='container' style={{ marginTop: "3em" }}>
+        {tables.map(table => {
+          a=table.status ? 'Occupied':"Available";
+          return (
+            <Row>
+              
+              <Card style={{ width: '23rem', marginTop: '2em' }} className="mx-auto">
+                <Card.Header></Card.Header>
+                <Card.Body>
+                  <Card.Title className='text-center'>Table {table.tableId}</Card.Title>
+                  <ListGroup variant="flush">
+                    <ListGroup.Item className="text-center" style={{
             backgroundColor: table.status ? 'red' : 'rgb(99, 194, 80)'
-          }} className='table'>{table.tableId}</div>
-        )
-      })}
+          }}>Status: {a}</ListGroup.Item>
+                  </ListGroup>
+                  <div className='d-flex justify-content-center' style={{ marginTop: '2em' }}>
+
+
+
+
+                  </div>
+
+                </Card.Body>
+              </Card>
+            </Row>
+
+
+
+          )
+        })}
+      </div>
+
+
     </div>
+
   )
 }
 
