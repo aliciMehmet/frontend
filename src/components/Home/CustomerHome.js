a2cc73499884656b9531548cd2425fe4f0d7dbfd
Mehmet Alıcı <alc_mehmet@outlook.com>
2022-05-30 10:29:04 +0300

    customerHome socket fix

--- src/components/Home/CustomerHome.js
+++ src/components/Home/CustomerHome.js
@@ -1,135 +1,98 @@
 import React, { useEffect, useState } from 'react'
-import { Card, Button } from 'react-bootstrap';
-import { Link, useSearchParams } from 'react-router-dom';
+import { useSearchParams } from 'react-router-dom';
 import { CustomerContext } from '../../context/CustomerContext';
 import { ItemContext } from '../../context/ItemContext';
 import ProductService from '../../services/ProductService';
 import Category from '../Category';
+import useWebSocket from 'react-use-websocket';
 
 function CustomerHome() {
 
-  function getQueryVariable(variable) {
-    var query = window.location.search.substring(1);
-    console.log(query)
-    var vars = query.split("&");
-    for (var i = 0; i < vars.length; i++) {
-      var pair = vars[i].split("=");
-      if (pair[0] == variable) { return pair[1]; }
-    }
-    return (false);
-  }
-
-
-
-  const [products, setProducts] = useState([])
-  const [businessId, setBusinessId] = useState(0)
-  const [tableId, setTableId] = useState(0)
-  const [categories, setCategories] = useState([])
-  const [socket, setSocket] = useState(null);
-
-
-  const { addProducts } = React.useContext(ItemContext)
-  const { loginCustomer } = React.useContext(CustomerContext)
-
-  useEffect(() => {
-    setBusinessId(getQueryVariable("businessId"));
-    setTableId(getQueryVariable("tableId"));
-
-    let customer = {
-      "tableId": tableId
+    function getQueryVariable(variable)
+    {
+            var query = window.location.search.substring(1);
+            console.log(query)
+            var vars = query.split("&");
+            for (var i=0;i<vars.length;i++) {
+                        var pair = vars[i].split("=");
+            if(pair[0] == variable){return pair[1];}
+             }
+             return(false);
     }
 
-    loginCustomer(customer)
+    const socketUrl = 'ws://localhost:8080/websocket';
 
-    !socket && setSocket(new WebSocket('ws://localhost:8080/websocket'))
+    const {
+        sendMessage,
+        sendJsonMessage,
+        lastMessage,
+        lastJsonMessage,
+        readyState,
+        getWebSocket,
+      } = useWebSocket(socketUrl, {
+        onOpen: () => {
+            let obj = {
+                "command":"SEATTABLE",
+                "tableId":getQueryVariable("tableId"),
+                "cafeId":getQueryVariable("businessId")
+            }
+            sendMessage(JSON.stringify(obj))
+        },
+        //Will attempt to reconnect on all close events, such as server shutting down
+        shouldReconnect: (closeEvent) => true,
+      });
 
-    var obj = {
-      "command": "SEATTABLE",
-      "cafeId": businessId,
-      "tableId": tableId
-    }
 
-    socket && socket.readyState == 1 && socket.send(JSON.stringify(obj));
+    const [products,setProducts] = useState([])
+    const [businessId,setBusinessId] = useState(0)
+    const [tableId,setTableId] = useState(0)
+    const [categories,setCategories] = useState([])
+    const [socket, setSocket] = useState(null);
 
-    console.log(socket)
 
-    let productService = new ProductService();
-    productService.getAllProducts(businessId).then(result => {
-      if (result.data != null) {
-        // setProducts(result.data.data)
+    const {addProducts} = React.useContext(ItemContext) 
+    const {loginCustomer} = React.useContext(CustomerContext) 
 
-        setCategories(Object.keys(result.data.data))
-        setProducts(result.data.data)
-        console.log(categories)
-        Object.keys(result.data.data).forEach(key => {
-          for (var i = 0; i < result.data.data[key].length; i++) {
-            addProducts(result.data.data[key][i])
-          }
+    useEffect(() => {
+        setBusinessId(getQueryVariable("businessId")); 
+        setTableId(getQueryVariable("tableId"));
 
+        let customer = {
+            "tableId":tableId
+        }
+
+        loginCustomer(customer)
+
+        let productService = new ProductService();
+        productService.getAllProducts(businessId).then(result => {
+            if(result.data != null){
+               // setProducts(result.data.data) 
+               
+             categories.length == 0 && setCategories(Object.keys(result.data.data))
+              setProducts(result.data.data)
+              console.log(result.data.data)
+              Object.keys(result.data.data).forEach(key =>{
+                  for(var i = 0; i < result.data.data[key].length; i++){
+                    addProducts(result.data.data[key][i])
+                  }
+                
+              })
+            }
         })
-      }
-    })
 
-
-
-
-  }, [socket]);
-
-
-  return (
-    <div >
-      {
-
-        categories.map(a => {
-          return (
             
-            <div>
-            <h1 className='text-center' style={{marginTop:'1em'}}>{a}'s</h1>  
-              {products[a].map(product => {
-                return (
+    }, [categories]);
 
-                  <div className='row d-flex justify-content-center'>
-                    <Card style={{ width: '23rem', marginTop: '2em' }}>
-                      <Card.Header></Card.Header>
-                      <Card.Body>
-                        <Card.Title className='text-center'>{product.name}</Card.Title>
-                        <Card.Text>
-                          
-                        </Card.Text>
-                        <div className='d-flex justify-content-center' style={{marginTop:'2em'}}>
-
-                        <Link to={"detail/"+product.id} key={product.id}>
-                        <Button variant="primary" className>Order</Button>
-                        </Link>
-                        
-                        </div>
-                        
-                      </Card.Body>
-                    </Card>
-
-                  </div>
-
-
-                )
-
+  
+    return(
+        <div>
+            {categories.map(a => {
+                 return <Category category={a} products = {products} key={a} />
               })}
-
-            </div>
-
-
-
-
-          )
-        })}
-
-
-
-    </div>
-
-
-  )
-
-
+        </div>
+    )
+    
+  
 }
 
 export default CustomerHome
